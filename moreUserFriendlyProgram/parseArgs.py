91ee5efeead7a7d60c05fd6fe8f2c2f83d0fff34
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2020-02-27 14:00:36 +0100

    Add files via upload

--- moreUserFriendlyProgram/parseArgs.py
+++ moreUserFriendlyProgram/parseArgs.py
@@ -14,98 +14,107 @@ import SegmentationShow
 
 
 def printHelpClasify():
-    print("HELP:")
+    print()
     print("Clasify with LBP: -test lbp -s otsu|gauss|mean -clasif ann|svm|clf|myclf -img b|g|r|all")
     print("Clasify with SobelLaplacian: -test sobel -clasif ann|svm|clf|myclf -img b|g|r|all")
-    print("Clasify with Wavelet: -test wavelet -t <type of wavelet, eg. db2> -s otsu|gauss|mean -clasif ann|svm|clf|myclf -img b|g|r|all")
+    print("Clasify with Wavelet: -test wavelet -t <wavelet type> -s otsu|gauss|mean -clasif ann|svm|clf|myclf -img b|g|r|all")
+    print("Show help for clasify: -test help")
+    print("Show general help: -help")
     print("Note - Only segmentation for Sobel Clasify is Otsu, because of better results")
     return
 
 def printHelpShow():
-    print("HELP:")
-    print("Show LBP img: -show lbp [-s otsu|gauss|mean] -img path")
-    print("Show SobelLaplacian img: -show sobel [-s otsu|gauss|mean] -img path")
-    print("Show Wavelet: -show wavelet -t <type of wavelet, eg. db2> [-s otsu|gauss|mean] -img path")
-    print("Show Segmented img: -show seg -s otsu|gauss|mean -img path")
+    print()
+    print("Show LBP img: -show lbp [-s otsu|gauss|mean] -img <path>")
+    print("Show SobelLaplacian img: -show sobel [-s otsu|gauss|mean] -img <path>")
+    print("Show Wavelet: -show wavelet -t <wavelet type> [-s otsu|gauss|mean] -img <path>")
+    print("Show Segmented img: -show seg -s otsu|gauss|mean -img <path>")
+    print("Show help for show: -show help")
+    print("Show general help: -help")
     print("Note - Segmentation argument -s is mandatory")
     return
 
-
-arguments_count = len(sys.argv) - 1
-if (sys.argv[1] == "-test"):
-    if (sys.argv[2] == "help"):
+def controlCorrectArguments(arguments_count):
+    if (arguments_count == 1 and (sys.argv[1] == "-help")):
         printHelpClasify()
-        exit(0)
-    elif ((sys.argv[2] != "lbp") and (sys.argv[2] != "sobel") and (sys.argv[2] != "wavelet")):
-        sys.stderr.write("ERROR - Use lbp, sobel, wavelet or help as argument for -test\n")
-        #printHelpClasify()
-        exit(1)
-
-    elif ((sys.argv[arguments_count] != "b") and (sys.argv[arguments_count] != "g") and (sys.argv[arguments_count] != "r")  and (sys.argv[arguments_count] != "all")):
-        sys.stderr.write("ERROR - Use b (all images with blue light), g (all images with green light), r (all images with red light) or all (mix of images with different lights) as argument for -img\n")
-        #printHelpClasify()
-        exit(1)
-
-    elif (sys.argv[2] == "wavelet" and ((sys.argv[3] != "-t") or (sys.argv[5] != "-s") or (sys.argv[7] != "-clasif") or (sys.argv[9] != "-img"))):
-        sys.stderr.write("ERROR - Use switches -t, -s, -clasif, -img for clasify with wavelet\n")
-        #printHelpClasify()
-        exit(1)
-
-    elif (sys.argv[2] == "lbp" and ((sys.argv[3] != "-s") or (sys.argv[5] != "-clasif") or (sys.argv[7] != "-img"))):
-        sys.stderr.write("ERROR - Use switches -s, -clasif, -img for clasify with lbp\n")
-        #printHelpClasify()
-        exit(1)
-
-    elif (sys.argv[2] == "sobel" and ((sys.argv[3] != "-clasif") or (sys.argv[5] != "-img"))):
-        sys.stderr.write("ERROR - Use switches -clasif, -img for clasify with sobel\n")
-        #printHelpClasify()
-        exit(1)
-    elif (sys.argv[2] == "wavelet" and arguments_count != 10):
-        sys.stderr.write("ERROR - Wrong count of arguments\n")
-        exit(1)
-    elif (sys.argv[2] == "lbp" and arguments_count != 8):
-        sys.stderr.write("ERROR - Wrong count of arguments\n")
-        exit(1)
-    elif (sys.argv[2] == "sobel" and arguments_count != 6):
-        sys.stderr.write("ERROR - Wrong count of arguments\n")
-        exit(1)
-
-    elif (sys.argv[arguments_count - 2] != "ann" and sys.argv[arguments_count - 2] != "svm" and sys.argv[arguments_count - 2] != "clf" and sys.argv[arguments_count - 2] != "myclf"):
-        sys.stderr.write("ERROR - Use paramatres for -clasif ann, svm, clf or myclf\n")
-        #printHelpClasify()
-        exit(1)
-
-    elif ((sys.argv[2] == "wavelet" or sys.argv[2] == "lbp") and(sys.argv[arguments_count - 4] != "otsu" and sys.argv[arguments_count - 4] != "gauss" and sys.argv[arguments_count - 4] != "mean")):
-        sys.stderr.write("ERROR - Use paramatres for -s otsu, gauss or mean\n")
-        #printHelpClasify()
-        exit(1)
-
-if (sys.argv[1] == "-show"):
-    if (sys.argv[2] == "help"):
         printHelpShow()
         exit(0)
-    elif ((sys.argv[2] != "lbp") and (sys.argv[2] != "sobel") and (sys.argv[2] != "wavelet") and (sys.argv[2] != "seg")):
-        sys.stderr.write("ERROR - Use lbp, sobel, wavelet or help as argument for -show\n")
+
+    elif (sys.argv[1] == "-test"):
+        if (arguments_count < 2 or arguments_count > 10):
+            sys.stderr.write("ERROR - Wrong count of arguments\n")
+            exit(1)
+        elif (sys.argv[2] == "help"):
+            printHelpClasify()
+            exit(0)
+        elif ((sys.argv[2] != "lbp") and (sys.argv[2] != "sobel") and (sys.argv[2] != "wavelet")):
+            sys.stderr.write("ERROR - Use lbp, sobel, wavelet or help as argument for -test\n")
+            exit(1)
+
+        elif ((sys.argv[arguments_count] != "b") and (sys.argv[arguments_count] != "g") and (sys.argv[arguments_count] != "r")  and (sys.argv[arguments_count] != "all")):
+            sys.stderr.write("ERROR - Use b (all images with blue light), g (all images with green light), r (all images with red light) or all (mix of images with different lights) as argument for -img\n")
+            exit(1)
+
+        elif (sys.argv[2] == "wavelet" and ((sys.argv[3] != "-t") or (sys.argv[5] != "-s") or (sys.argv[7] != "-clasif") or (sys.argv[9] != "-img"))):
+            sys.stderr.write("ERROR - Use switches -t, -s, -clasif, -img for clasify with wavelet\n")
+            exit(1)
+
+        elif (sys.argv[2] == "lbp" and ((sys.argv[3] != "-s") or (sys.argv[5] != "-clasif") or (sys.argv[7] != "-img"))):
+            sys.stderr.write("ERROR - Use switches -s, -clasif, -img for clasify with lbp\n")
+            exit(1)
+
+        elif (sys.argv[2] == "sobel" and ((sys.argv[3] != "-clasif") or (sys.argv[5] != "-img"))):
+            sys.stderr.write("ERROR - Use switches -clasif, -img for clasify with sobel\n")
+            exit(1)
+        elif (sys.argv[2] == "wavelet" and arguments_count != 10):
+            sys.stderr.write("ERROR - Wrong count of arguments\n")
+            exit(1)
+        elif (sys.argv[2] == "lbp" and arguments_count != 8):
+            sys.stderr.write("ERROR - Wrong count of arguments\n")
+            exit(1)
+        elif (sys.argv[2] == "sobel" and arguments_count != 6):
+            sys.stderr.write("ERROR - Wrong count of arguments\n")
+            exit(1)
+
+        elif (sys.argv[arguments_count - 2] != "ann" and sys.argv[arguments_count - 2] != "svm" and sys.argv[arguments_count - 2] != "clf" and sys.argv[arguments_count - 2] != "myclf"):
+            sys.stderr.write("ERROR - Use paramatres for -clasif ann, svm, clf or myclf\n")
+            exit(1)
+
+        elif ((sys.argv[2] == "wavelet" or sys.argv[2] == "lbp") and(sys.argv[arguments_count - 4] != "otsu" and sys.argv[arguments_count - 4] != "gauss" and sys.argv[arguments_count - 4] != "mean")):
+            sys.stderr.write("ERROR - Use paramatres for -s otsu, gauss or mean\n")
+            exit(1)
+
+    elif (sys.argv[1] == "-show"):
+        if (arguments_count < 2 or arguments_count > 8):
+            sys.stderr.write("ERROR - Wrong count of arguments\n")
+            exit(1)
+        elif (sys.argv[2] == "help" and arguments_count == 2):
+            printHelpShow()
+            exit(0)
+        elif ((sys.argv[2] != "lbp") and (sys.argv[2] != "sobel") and (sys.argv[2] != "wavelet") and (sys.argv[2] != "seg")):
+            sys.stderr.write("ERROR - Use lbp, sobel, wavelet or help as argument for -show\n")
+            exit(1)
+
+        elif(sys.argv[arguments_count - 1] != "-img"):
+            sys.stderr.write("ERROR - Use -img for img path in -show\n")
+            exit(1)
+
+        elif (sys.argv[2] == "wavelet" and (sys.argv[3] != "-t")):
+            sys.stderr.write("ERROR - Use compulsory -t for type of wavelet for show wavelet img\n")
+            exit(1)
+
+        elif ("-s" in sys.argv and (("otsu" not in sys.argv) and ("gauss" not in sys.argv) and ("mean" not in sys.argv))):
+            sys.stderr.write("ERROR - Use otsu, gauss or mean with -s (segmentation)\n")
+            exit(1)
+    else:
+        sys.stderr.write("ERROR - Use -show, -help or -test, for more information see -help.\n")
         exit(1)
 
-    elif(sys.argv[arguments_count - 1] != "-img"):
-        sys.stderr.write("ERROR - Use -img for img path in -show\n")
-        exit(1)
 
-
-    elif (sys.argv[2] == "wavelet" and (sys.argv[3] != "-t")):
-        sys.stderr.write("ERROR - Use compulsory -t for type of wavelet for show wavelet img\n")
-        exit(1)
-
-    elif ("-s" in sys.argv and (("otsu" not in sys.argv) and ("gauss" not in sys.argv) and ("mean" not in sys.argv))):
-        sys.stderr.write("ERROR - Use otsu, gauss or mean with -s (segmentation)\n")
-        exit(1)
-
-
-
+arguments_count = len(sys.argv) - 1
+controlCorrectArguments(arguments_count)
 
 if (sys.argv[1] == "-show"):
-    print("To do")
     input_img = sys.argv[arguments_count]
     print(input_img)
     # SHOW LBP
